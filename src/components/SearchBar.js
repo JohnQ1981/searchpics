12fa9ee6fa9a468d1972f5c411680b08f62c5206
JohnQ1981 <ikram1981@gmail.com>
2022-09-25 13:25:36 -0500

    added toUpperCase()

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,8 +4,8 @@ class SearchBar extends React.Component {
   //   onInputChange(event) {
   //     console.log(event.target.value);
   //   }
-  state = { term: "" };
-
+  state = { term: "search here" };
+    
   render() {
     return (
       <div className="ui segment">
@@ -15,8 +15,9 @@ class SearchBar extends React.Component {
             <input
               type="text"
               value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
+              onChange={e => this.setState({ term: e.target.value.toUpperCase() })}
             />
+            
           </div>
         </form>
       </div>
