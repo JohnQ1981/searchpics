b9ea7003064bba64a90d5defecfc9e814ac3c05d
JohnQ1981 <ikram1981@gmail.com>
2022-09-25 12:55:26 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ImageList from "./components/ImageList";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container" style={{marginTop: '10px'}}>
+      
+      <ImageList />
+      <hr></hr>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ImageList = () => {
+  return (
+    <div>
+      Hi from Image list
+      <div>Hi</div>
+    </div>
+  );
+};
+
+export default ImageList;
